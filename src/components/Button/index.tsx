b1dd80b4539df70218c3279f5c57f8e77c92c955
HamSungJun <tjdwns5123@gmail.com>
2022-12-27 21:35:42 +0900

    123

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,4 @@
-import { useEffect } from "react";
-
 export default function Button() {
-  const a = 1;
   return (
     <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
       Button
